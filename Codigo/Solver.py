edcb8a99ae2a59532eb2e502d690b54d1f772651
AgustinCorazza <corazzaagustin@gmail.com>
2024-07-13 22:43:56 +0200

    Added Styles

--- Codigo/Solver.py
+++ Codigo/Solver.py
@@ -1,10 +1,13 @@
 import heapq
-from PyQt5.QtCore import QTimer
-import math
+from PyQt5.QtCore import QTimer, pyqtSignal, QObject
+from datetime import datetime
 
 
-class Solver:
+class Solver(QObject):
+    solve_completed = pyqtSignal()
+
     def __init__(self, maze):
+        super().__init__()
         self.maze = maze
         self.openSet = []
         self.cameFrom = {}
@@ -14,6 +17,9 @@ class Solver:
         self.timer = QTimer()
         self.timer.timeout.connect(self.step)
         self.endCell = None
+        self.visited_count = 0
+        self.start_time = 0
+        self.end_time = 0
 
     def heuristic(self, current, target):
         raise NotImplementedError("Subclasses should implement heuristic.")
@@ -66,6 +72,8 @@ class AStarSolver(Solver):
             raise TypeError("Score values should be either a list or an integer")
 
     def solve(self):
+        self.visited_count = 0
+        self.start_time = datetime.now()
         start = self.maze.startCell
         self.endCell = self.maze.endCell
 
@@ -90,6 +98,7 @@ class AStarSolver(Solver):
         # Pop first element of the queue (node with minimum value of f_score)
         _, current = heapq.heappop(self.openSet)
 
+        self.visited_count += 1
         self.maze.addClosedSetNode(*current)
         self.controller.maze_widget.update()
         for neighbor in self.get_neighbors(current):
@@ -98,9 +107,13 @@ class AStarSolver(Solver):
         for neighbor in self.get_neighbors(current):
             if neighbor == self.endCell:  # Skip further exploration if neighbor is the destination
                 self.timer.stop()
+                self.end_time = datetime.now()
                 self.cameFrom[neighbor] = current
                 path = self.reconstruct_path(current)
                 self.controller.maze_widget.path = path
+                self.controller.maze_widget.path_stats["N_Visited"] = self.visited_count
+                self.controller.maze_widget.path_stats["Time_Spent"] = self.end_time - self.start_time
+                self.solve_completed.emit()
                 self.controller.maze_widget.update()
                 return
 
@@ -116,7 +129,6 @@ class AStarSolver(Solver):
                 # Adds the neighbor to the priority queue (open set) with its f_score as the priority.
                 # The priority queue ensures nodes with the lowest f_score are explored first.
                 heapq.heappush(self.openSet, (self.f_score[neighbor], neighbor))
-
 
 
 class ModifiedAStarSolver(Solver):
@@ -136,6 +148,8 @@ class ModifiedAStarSolver(Solver):
             raise TypeError("Score values should be either a list or an integer")
 
     def solve(self):
+        self.visited_count = 0
+        self.start_time = datetime.now()
         start = self.maze.startCell
         self.endCell = self.maze.endCell
 
@@ -160,6 +174,7 @@ class ModifiedAStarSolver(Solver):
         # Pop first element of the queue (node with minimum value of f_score)
         _, current = heapq.heappop(self.openSet)
 
+        self.visited_count += 1
         self.maze.addClosedSetNode(*current)
         self.controller.maze_widget.update()
         for neighbor in self.get_neighbors(current):
@@ -168,9 +183,13 @@ class ModifiedAStarSolver(Solver):
         for neighbor in self.get_neighbors(current):
             if neighbor == self.endCell:  # Skip further exploration if neighbor is the destination
                 self.timer.stop()
+                self.end_time = datetime.now()
                 self.cameFrom[neighbor] = current
                 path = self.reconstruct_path(current)
                 self.controller.maze_widget.path = path
+                self.controller.maze_widget.path_stats["N_Visited"] = self.visited_count
+                self.controller.maze_widget.path_stats["Time_Spent"] = self.end_time - self.start_time
+                self.solve_completed.emit()
                 self.controller.maze_widget.update()
                 return
 
@@ -190,6 +209,7 @@ class ModifiedAStarSolver(Solver):
                 self.maze.addOpenSetNode(*neighbor)
                 self.controller.maze_widget.update()
 
+
 class DijkstraSolver(Solver):
     def __init__(self, maze):
         super().__init__(maze)
@@ -208,6 +228,8 @@ class DijkstraSolver(Solver):
             raise TypeError("Score values should be either a list or an integer")
 
     def solve(self):
+        self.visited_count = 0
+        self.start_time = datetime.now()
         start = self.maze.startCell
         self.endCell = self.maze.endCell
 
@@ -232,6 +254,7 @@ class DijkstraSolver(Solver):
         # Pop first element of the queue (node with minimum value of f_score)
         _, current = heapq.heappop(self.openSet)
 
+        self.visited_count += 1
         self.maze.addClosedSetNode(*current)
         self.controller.maze_widget.update()
         for neighbor in self.get_neighbors(current):
@@ -240,78 +263,13 @@ class DijkstraSolver(Solver):
         for neighbor in self.get_neighbors(current):
             if neighbor == self.endCell:  # Skip further exploration if neighbor is the destination
                 self.timer.stop()
+                self.end_time = datetime.now()
                 self.cameFrom[neighbor] = current
                 path = self.reconstruct_path(current)
                 self.controller.maze_widget.path = path
-                self.controller.maze_widget.update()
-                return
-
-            # Get cost from the start node to the current node (defaulting to infinity if it's not found),
-            # adds the cost of moving from the current node to the neighbor which is assumed to be 1
-            tentative_g_score = self.g_score.get(current, float('inf')) + 1
-
-            if tentative_g_score < self.g_score.get(neighbor, float('inf')):
-                self.cameFrom[neighbor] = current
-                self.g_score[neighbor] = tentative_g_score
-                self.f_score[neighbor] = self.eval_function([self.g_score[neighbor]])
-                # Adds the neighbor to the priority queue (open set) with its f_score as the priority.
-                # The priority queue ensures nodes with the lowest f_score are explored first.
-                heapq.heappush(self.openSet, (self.f_score[neighbor], neighbor))
-                self.maze.addOpenSetNode(*neighbor)
-                self.controller.maze_widget.update()
-
-
-class DijkstraSolver(Solver):
-    def __init__(self, maze):
-        super().__init__(maze)
-
-    def heuristic(self, a, b):
-        # Dijkstra's algorithm does not use a heuristic
-        return 0
-
-    def eval_function(self, values):
-        # Dijkstra's algorithm uses only the g_score
-        if isinstance(values, list):
-            return values[0]
-        elif isinstance(values, int):
-            return values
-        else:
-            raise TypeError("Score values should be either a list or an integer")
-
-    def solve(self):
-        start = self.maze.startCell
-        self.endCell = self.maze.endCell
-
-        if not start or not self.endCell:
-            return None
-
-        self.openSet = []
-        # Push start node into the priority queue
-        heapq.heappush(self.openSet, (0, start))
-        self.cameFrom = {}
-
-        self.g_score = {start: 0}  # Traveled distance from starting node to the starting node is zero
-        self.f_score = {start: self.eval_function([self.g_score[start]])}
-
-        self.maze.clearSetNodes()
-        self.timer.start(10)  # Adjust the interval as needed
-
-    def step(self):
-        if not self.openSet:
-            self.timer.stop()
-            return
-        # Pop first element of the queue (node with minimum value of f_score)
-        _, current = heapq.heappop(self.openSet)
-
-        self.maze.addClosedSetNode(*current)
-        self.controller.maze_widget.update()
-
-        for neighbor in self.get_neighbors(current):
-            if neighbor == self.endCell:  # Skip further exploration if neighbor is the destination
-                self.timer.stop()
-                self.cameFrom[neighbor] = current
-                path = self.reconstruct_path(current)
-                self.controller.maze_widget.path = path
+                self.controller.maze_widget.path_stats["N_Visited"] = self.visited_count
+                self.controller.maze_widget.path_stats["Time_Spent"] = self.end_time - self.start_time
+                self.solve_completed.emit()
                 self.controller.maze_widget.update()
                 return
 
--- Codigo/main_window.py
+++ Codigo/main_window.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QMainWindow, QVBoxLayout, QPushButton, QRadioButton, QHBoxLayout, QButtonGroup, \
-    QDesktopWidget
+    QDesktopWidget, QLabel
 from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt
 from Solver import AStarSolver, DijkstraSolver, ModifiedAStarSolver
@@ -16,6 +16,8 @@ class MazeWidget(QWidget):
         self.cell_size = 20
         self.drawing_wall = False
         self.closedSet = None
+        self.path_stats = {}
+        self.setMinimumSize(800, 800)  # Set a minimum size for the MazeWidget
 
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -71,7 +73,7 @@ class MazeWidget(QWidget):
             columnID = widget_pos.x() // self.cell_size
             rowID = widget_pos.y() // self.cell_size
 
-            if 0 <= rowID < self.maze.nbRows and  0 <= columnID < self.maze.nbColumns:
+            if 0 <= rowID < self.maze.nbRows and 0 <= columnID < self.maze.nbColumns:
                 if self.maze.grid[rowID][columnID] == 0:
                     self.maze.setWallCell(rowID, columnID)
                     self.update()
@@ -85,31 +87,53 @@ class MazeWidget(QWidget):
         self.path = []
         self.update()
 
+
 class MainWindow(QMainWindow):
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
 
-        self.setWindowTitle("Maze Solver")
+        self.setWindowTitle("Maze Generator & Solver")
         # Get screen resolution
         screen_resolution = QDesktopWidget().screenGeometry()
-        width, height = int(screen_resolution.width()*0.80), int(screen_resolution.height()*0.85)
+        width, height = int(screen_resolution.width() * 0.80), int(screen_resolution.height() * 0.85)
+
+        title_font = QFont("Arial", 15)
+        normal_font = QFont("Arial", 10)
 
         self.setGeometry(150, 100, width, height)
 
         self.maze_widget = MazeWidget(self.controller.maze, self.controller)
         self.solve_button = QPushButton("Solve Maze")
+        self.solve_button.setFont(normal_font)
+        self.solve_button.setStyleSheet("background-color: #CCD2CD;")
         self.solve_button.clicked.connect(self.controller.solve_maze)
 
         self.generate_button = QPushButton("Generate Maze")
+        self.generate_button.setFont(normal_font)
+        self.generate_button.setStyleSheet("background-color: #CCD2CD;")
         self.generate_button.clicked.connect(self.generate_maze)
 
         self.clear_button = QPushButton("Clear Maze")
+        self.clear_button.setFont(normal_font)
+        self.clear_button.setStyleSheet("background-color: #CCD2CD;")
         self.clear_button.clicked.connect(self.clear_maze)
 
+        self.title_label = QLabel("Maze Generator & Solver")
+        self.title_label.setFont(title_font)
+        self.title_label.setContentsMargins(0, 0, 0, 10)
+
+        self.solver_label = QLabel("Choose Maze Solver: ")
+        self.solver_label.setFont(normal_font)
+        self.solver_label.setContentsMargins(5, 5, 0, 0)
+
         self.astar_radio = QRadioButton("A* Solver")
+        self.astar_radio.setFont(normal_font)
         self.dijkstra_radio = QRadioButton("Dijkstra Solver")
+        self.dijkstra_radio.setFont(normal_font)
         self.modified_astar_radio = QRadioButton("Modified A* Solver")
+        self.modified_astar_radio.setFont(normal_font)
+
         self.astar_radio.setChecked(True)
 
         self.radio_group = QButtonGroup()
@@ -121,17 +145,25 @@ class MainWindow(QMainWindow):
         radio_layout.addWidget(self.astar_radio)
         radio_layout.addWidget(self.dijkstra_radio)
         radio_layout.addWidget(self.modified_astar_radio)
+        radio_layout.setContentsMargins(20, 10, 10, 30)
 
         left_layout = QVBoxLayout()
+        left_layout.setContentsMargins(30, 0, 30, 0)
         left_layout.setAlignment(Qt.AlignTop)
+        left_layout.addWidget(self.title_label, alignment=Qt.AlignCenter)
+        left_layout.addWidget(self.solver_label)
         left_layout.addLayout(radio_layout)
         left_layout.addWidget(self.solve_button)
         left_layout.addWidget(self.generate_button)
         left_layout.addWidget(self.clear_button)
-        main_layout = QHBoxLayout()
 
+        main_layout = QHBoxLayout()
         main_layout.addLayout(left_layout, stretch=2)
-        main_layout.addWidget(self.maze_widget, stretch=4)
+
+        # Create a centered layout for the maze widget
+        centered_layout = QVBoxLayout()
+        centered_layout.addWidget(self.maze_widget, alignment=Qt.AlignCenter)
+        main_layout.addLayout(centered_layout, stretch=4)
 
         container = QWidget()
         container.setLayout(main_layout)
@@ -143,6 +175,23 @@ class MainWindow(QMainWindow):
         self.dijkstra_radio.toggled.connect(self.on_solver_changed)
         self.modified_astar_radio.toggled.connect(self.on_solver_changed)
 
+        self.setStyleSheet("background-color: lightblue;")
+
+        # Add label to show path_stats
+        self.path_stats_label = QLabel()
+        self.path_stats_label.setContentsMargins(15, 10, 0, 0)
+        self.path_stats_label.setFont(normal_font)
+        left_layout.addWidget(self.path_stats_label)
+
+        self.controller.solver.solve_completed.connect(self.update_path_stats_label)
+
+    def update_path_stats_label(self):
+        path_stats = self.maze_widget.path_stats
+        text = f"""Solver Information: 
+        \tNumber of visited cells: {path_stats["N_Visited"]}
+        \tTime Spent: {path_stats["Time_Spent"]}"""
+        self.path_stats_label.setText(text)
+
     def get_selected_solver(self):
         if self.astar_radio.isChecked():
             return AStarSolver(self.controller.maze)
@@ -153,6 +202,7 @@ class MainWindow(QMainWindow):
 
     def on_solver_changed(self):
         self.controller.set_solver(self.get_selected_solver())
+        self.controller.solver.solve_completed.connect(self.update_path_stats_label)
 
     def solve_maze(self):
         self.controller.solve_maze()
